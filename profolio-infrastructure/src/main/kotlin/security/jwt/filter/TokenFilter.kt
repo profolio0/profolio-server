120a15b8aebf531bf71378f758d4f8c732855d53
kmg <legolove08@naver.com>
2024-12-28 14:18:48 +0900

    docs: todo

--- profolio-infrastructure/src/main/kotlin/security/jwt/filter/TokenFilter.kt
+++ profolio-infrastructure/src/main/kotlin/security/jwt/filter/TokenFilter.kt
@@ -1,4 +1,16 @@
 package security.jwt.filter
 
-class TokenFilter {
+import jakarta.servlet.FilterChain
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.web.filter.OncePerRequestFilter
+
+class TokenFilter: OncePerRequestFilter() {
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        TODO("토큰 인증 구현")
+    }
 }
