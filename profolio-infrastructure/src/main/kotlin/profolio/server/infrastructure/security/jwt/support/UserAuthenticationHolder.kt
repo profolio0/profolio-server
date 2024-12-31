c6228c084831396b7953f7cdb2106cc24f07770d
kmg <legolove08@naver.com>
2024-12-31 09:55:12 +0900

    feat: authenticationHolder

--- profolio-infrastructure/src/main/kotlin/profolio/server/infrastructure/security/jwt/support/UserAuthenticationHolder.kt
+++ profolio-infrastructure/src/main/kotlin/profolio/server/infrastructure/security/jwt/support/UserAuthenticationHolder.kt
@@ -2,11 +2,12 @@ package profolio.server.infrastructure.security.jwt.support
 
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Component
+import profolio.server.domain.rds.user.entity.User
 import profolio.server.domain.rds.user.entity.UserDetails
 
 @Component
 object UserAuthenticationHolder {
-    fun current() {
-//        return (SecurityContextHolder.getContext().authentication.principal as UserDetails).user
+    fun current(): User {
+        return (SecurityContextHolder.getContext().authentication.principal as UserDetails).user
     }
 }
